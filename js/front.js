a8ed136e53e019fb0aa1c0747a35cc1fab7eed97
rensramos <rensramos17@gmail.com>
2015-06-29 11:43:21 +0800

    add navbar collapse on mobile devices

--- js/front.js
+++ js/front.js
@@ -1,25 +1,25 @@
-var rd = rd || {};
-  rd.init = function() {
-      rd.addActiveClass();
-  }
-
-rd.addActiveClass = function () {
-  $(".navbar-nav>li").on('click', "a", function(){
-    $(".navbar-nav .active").removeClass("active");
-    $(this).addClass("active");
-  });
-};
-
-//$(document).ready(function () {
-//    $("ul.art-hmenu>li").on("click", "a", function (event) {
-//        debugger;
-//        $("#menu_wrapper .active").removeClass("active");
-//        $(this).addClass("active");
-//    });
+//var rd = rd || {};
+//  rd.init = function() {
+//      rd.addActiveClass();
+//  }
+//
+//rd.addActiveClass = function () {
+//  $(".navbar-nav>li").click(function(){
+//    $(".navbar-nav>li .active").removeClass("active");
+//    $(this).addClass("active");
+//  });
+//};
+//
+////$(document).ready(function () {
+////    $("ul.art-hmenu>li").on("click", "a", function (event) {
+////        debugger;
+////        $("#menu_wrapper .active").removeClass("active");
+////        $(this).addClass("active");
+////    });
+////});
+//
+//
+//
+//$(function(){
+//    rd.init();
 //});
-
-
-
-$(function(){
-    rd.init();
-});
