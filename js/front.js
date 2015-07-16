345ae596281e526a5938daa0f28a1645af5f7b15
rensramos <rensramos17@gmail.com>
2015-07-16 20:31:53 +0800

    update

--- js/front.js
+++ js/front.js
@@ -1,25 +1,16 @@
-//var rd = rd || {};
-//  rd.init = function() {
-//      rd.addActiveClass();
-//  }
-//
-//rd.addActiveClass = function () {
-//  $(".navbar-nav>li").click(function(){
-//    $(".navbar-nav>li .active").removeClass("active");
-//    $(this).addClass("active");
-//  });
-//};
-//
-////$(document).ready(function () {
-////    $("ul.art-hmenu>li").on("click", "a", function (event) {
-////        debugger;
-////        $("#menu_wrapper .active").removeClass("active");
-////        $(this).addClass("active");
-////    });
-////});
-//
-//
-//
-//$(function(){
-//    rd.init();
-//});
+var fs = fs || {};
+  fs.init = function() {
+      fs.addActiveClass();
+  }
+
+fs.addActiveClass = function () {
+  var $navbarMenu = $('.navbar-nav>li a');
+  $navbarMenu.on('click', function(){
+    $navbarMenu.removeClass('active');
+    $(this).addClass('active');
+  });
+};
+
+$(function(){
+    fs.init();
+});
