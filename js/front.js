cc0e9546ff9692be9f812093faaa3a94cded5a85
rensramos <rensramos17@gmail.com>
2015-07-20 16:23:12 +0800

    update carousel

--- js/front.js
+++ js/front.js
@@ -1,6 +1,7 @@
 var fs = fs || {};
   fs.init = function() {
       fs.addActiveClass();
+      fs.carouselImg();
   }
 
 fs.addActiveClass = function () {
@@ -9,6 +10,15 @@ fs.addActiveClass = function () {
     $navbarMenu.removeClass('active');
     $(this).addClass('active');
   });
+};
+
+fs.carouselImg = function(){
+    $('.carousel').carousel({
+        interval: 3000
+
+    });
+    $('.carousel-indicators').find('li').css('display','none');
+    $('.carousel-inner .item:first-child').addClass('active');
 };
 
 $(function(){
