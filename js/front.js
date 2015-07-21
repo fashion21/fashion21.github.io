aeef562cb39340e251314fdba1c0dfcfa58cab13
rensramos <rensramos17@gmail.com>
2015-07-21 20:31:20 +0800

    add instagram widget

--- js/front.js
+++ js/front.js
@@ -5,7 +5,15 @@ var fs = fs || {};
       fs.categoryItems();
       fs.facebookPageWidget();
       fs.facebookLikeWidget();
+      fs.instagramWidget();
   }
+
+fs.instagramWidget = function(){
+    $.getScript('http://instagramfollowbutton.com/components/instagram/v2/js/ig-follow.js', function(d,t){
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        s.parentNode.insertBefore(g,s);
+    }(document,"script"));
+};
 
 fs.facebookLikeWidget = function(){
     (function(d, s, id) {
