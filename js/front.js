274b4c42b28b73040048595bceaad68b302ee30c
rensramos <rensramos17@gmail.com>
2015-07-21 20:17:02 +0800

    add facebook widget

--- js/front.js
+++ js/front.js
@@ -4,7 +4,18 @@ var fs = fs || {};
       fs.carouselImg();
       fs.categoryItems();
       fs.facebookPageWidget();
+      fs.facebookLikeWidget();
   }
+
+fs.facebookLikeWidget = function(){
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+};
 
 fs.facebookPageWidget = function(){
     (function(d, s, id) {
