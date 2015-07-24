02a65101564af900cbda660cb3d1da755bb8cb76
rensramos <rensramos17@gmail.com>
2015-07-24 11:41:04 +0800

    add function

--- js/productCategory.js
+++ js/productCategory.js
@@ -5,12 +5,29 @@ fs.init = function() {
 
 fs.navbarToggle = function(){
     $('.navbar-header').on('click','button', function() {
-        alert('meow');
-        //$navbarGroup.find('.collapse.in').collapse('hide');
+        //if($('#navbar-category-collapse .navbar-collapse .collapse').hasClass('in')){
+        //    alert('meow');
+        //    $('#navbar-menu-collapse .navbar-collapse .collapse.in').removeClass('in');
+        //}
+        //else if($('#navbar-menu-collapse .navbar-collapse .collapse').hasClass('in')){
+        //    $('#navbar-category-collapse .navbar-collapse .colapse.in').removeClass('in');
+        //    alert('meow');
+        //}
+        var categoryButton = $("#category-button");
+        var menuButton = $("#menu-button");
+        if($(this)[0] == categoryButton[0]){
+            if($("#navbar-menu-collapse").hasClass("navbar-collapse collapse in")){
+                menuButton.click();
+            }
+        }
+        else if($(this)[0] == menuButton[0]){
+            if($("#navbar-category-collapse").hasClass("navbar-collapse collapse in")){
+                categoryButton.click();
+            }
+        }
     });
 
 };
-
 
 
 $(function(){
