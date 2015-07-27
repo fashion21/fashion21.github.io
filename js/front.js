868700d8df616c8f7a1e7a6176f43ce24ff643c9
rensramos <rensramos17@gmail.com>
2015-07-27 12:13:24 +0800

    move social media js

--- js/front.js
+++ js/front.js
@@ -28,25 +28,6 @@ fs.categoryItemHover = function(){
     });
 };
 
-
-
-fs.instagramWidget = function(){
-    $.getScript('http://instagramfollowbutton.com/components/instagram/v2/js/ig-follow.js', function(d,t){
-        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        s.parentNode.insertBefore(g,s);
-    }(document,"script"));
-};
-
-fs.facebookLikeWidget = function(){
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-};
-
 fs.facebookPageWidget = function(){
     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
