bd3e93f8bc90911c65bddedbc3f567ed46c548cb
rensramos <rensramos17@gmail.com>
2015-07-30 19:43:59 +0800

    update imageSlider control

--- js/lightImageSlider.js
+++ js/lightImageSlider.js
@@ -6,7 +6,7 @@ fs.imageSlider = function() {
 fs.productImageSlider = function(){
     var autoPlaySlider = $('#product-img-slider').lightSlider({
         item: 5,
-        controls: false,
+        controls: true,
         speed: 2000,
         pause: 4000,
         auto: true,
